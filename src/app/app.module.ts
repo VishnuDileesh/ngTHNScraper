a45502457ebe0bc39d93809323c08f8959ca2a3a
Vishnu Dileesh <dev@vishnudileesh.com>
2020-08-06 19:45:02 +0530

    Add - category cards header section ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { NavheaderComponent } from './navheader/navheader.component';
+import { CatcardComponent } from './catcard/catcard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavheaderComponent,
+    CatcardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/catcard/catcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catcard',
+  templateUrl: './catcard.component.html',
+  styleUrls: ['./catcard.component.css']
+})
+export class CatcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
